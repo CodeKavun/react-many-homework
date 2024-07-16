f293de96af1c19a14ed6ff0f40046a394cbf80f6
PanDmytryk <dimacapny22@gmail.com>
2024-07-16 16:34:20 +0200

    Add hooks practice project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
+import { useState } from 'react';
 import './App.css';
-import FilmComponent from './FilmComponent';
+
+function CounterButton(props) {
+	const changeCounter = () => {
+		props.onCounterClick(props.valueIncrement)
+	}
+
+	return (
+		<button onClick={changeCounter}>{props.text}</button>
+	)
+}
 
 function App() {
-  return (
-    <div className="App">
-      <FilmComponent title="Rambo - First Blood" director="Ted Kotcheff"
-        year="1982" studio="Orion Pictures" actors={["Silwester Stalone", "Richard Crenna",
-        "Brian Dennehy", "David Caruso", "Michael Talbott"]} ></FilmComponent>
-    </div>
-  );
+	const [counter, setCounter] = useState(0);
+	const stateFunction = (value) => {
+		setCounter(parseInt(counter) + value);
+	}
+
+  	return (
+    	<>
+		<CounterButton text="+10" valueIncrement={10} onCounterClick={stateFunction}></CounterButton>
+		<CounterButton text="-100" valueIncrement={-100} onCounterClick={stateFunction}></CounterButton>
+		<CounterButton text="+25" valueIncrement={25} onCounterClick={stateFunction}></CounterButton>
+		<h1>{counter}</h1>
+    	</>
+  	);
 }
 
 export default App;
